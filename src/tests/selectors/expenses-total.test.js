10162a43409f78e8e841a3fe022f0d1e34990189
srijita kiki <kikisrijita@gmail.com>
2018-08-05 23:15:12 +0600

    Setup test database env

--- src/tests/selectors/expenses-total.test.js
+++ src/tests/selectors/expenses-total.test.js
@@ -3,7 +3,7 @@ import expenses from '../fixtures/expenses';
 
 test('should return 0 if no expenses', () => {
   const res = selectExpensesTotal([]);
-  expect(res).toBe(0);
+  expect(res).toBe(0); 
 });
 
 test('should correctly add up a single expense', () => {
