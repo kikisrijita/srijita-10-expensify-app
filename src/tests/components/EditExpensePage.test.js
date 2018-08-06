bb62a97fcf4fdfda59ae045bf0e866cdfd24b5f9
srijita kiki <kikisrijita@gmail.com>
2018-08-07 00:16:57 +0600

    setup start remove expense

--- src/tests/components/EditExpensePage.test.js
+++ src/tests/components/EditExpensePage.test.js
@@ -4,16 +4,16 @@ import expenses from '../fixtures/expenses'
 import {EditExpensePage} from '../../components/EditExpensePage';
 import ExpenseForm from '../../components/ExpenseForm';
 
-let editExpense, removeExpense, history, wrapper ;
+let editExpense,  startRemoveExpense, history, wrapper ;
 
 beforeEach(()=> {
   editExpense = jest.fn()
-  removeExpense = jest.fn()
+  startRemoveExpense = jest.fn()
   history = { push: jest.fn()}
   wrapper = shallow(
     <EditExpensePage 
         editExpense = {editExpense}
-        removeExpense = {removeExpense}
+        startRemoveExpense = { startRemoveExpense}
         history = {history}
         expense = {expenses[2]}
     />
@@ -30,8 +30,8 @@ test('should handle editExpense correctly', () => {
   expect(editExpense).toHaveBeenCalledWith(expenses[2].id, expenses[2])
 });
 
-test('should handle removeExpense correctly', () => {
+test('should handle  startRemoveExpense correctly', () => {
   wrapper.find('button').simulate('click')
   expect(history.push).toHaveBeenCalledWith('/')
-  expect(removeExpense).toHaveBeenCalledWith({id: expenses[2].id})
+  expect( startRemoveExpense).toHaveBeenCalledWith({id: expenses[2].id})
 });
